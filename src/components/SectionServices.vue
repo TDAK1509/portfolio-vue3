9385fb808591db75d69288163fb2047e22cb79c5
Khuong Tran <dbcgdhh@gmail.com>
2024-10-29 10:04:04 +0700

    Finish content of services

--- src/components/SectionServices.vue
+++ src/components/SectionServices.vue
@@ -4,10 +4,37 @@
       <template #title>What I can do for you</template>
     </SectionTitle>
 
-    <div class="block">
-      <ul class="disc">
-        <li>Lead engineer</li>
-        <li>Senior frontend development</li>
+    <div class="card">
+      <SvgArchitecture class="card-image" />
+      <h5>Lead engineer</h5>
+      <ul class="ul disc">
+        <li class="li">Decide architecture</li>
+        <li class="li">Create automated testing systems</li>
+        <li class="li">Create deployment workflows</li>
+        <li class="li">Create ways for team to work together efficiently</li>
+        <li class="li">Mentorship, onboarding and training</li>
+        <li class="li">
+          Review team members' works with constructive feedbacks
+        </li>
+        <li class="li">Project management, documenting</li>
+        <li class="li">
+          Active communication with all related departments and people.
+        </li>
+      </ul>
+    </div>
+
+    <div class="card">
+      <SvgCode class="card-image" />
+      <h5>Senior frontend development</h5>
+      <ul class="ul disc">
+        <li class="li">Create automated testing systems</li>
+        <li class="li">Create CIs system</li>
+        <li class="li">Configure Git repository to be easily used by others</li>
+        <li class="li">Documenting</li>
+        <li class="li">Mentorship, onboarding and training</li>
+        <li class="li">Have a deep understanding about the project</li>
+        <li class="li">Solve complex problems</li>
+        <li class="li">Communication with team members and other teams.</li>
       </ul>
     </div>
   </section>
@@ -15,6 +42,8 @@
 
 <script setup>
 import SectionTitle from "@/components/SectionTitle.vue";
+import SvgArchitecture from "@/components/SvgArchitecture.vue";
+import SvgCode from "@/components/SvgCode.vue";
 </script>
 
 <style scoped>
@@ -22,8 +51,21 @@ import SectionTitle from "@/components/SectionTitle.vue";
   text-align: center;
   padding: 0 1em;
 }
-.block {
-  width: min(300px, 80%);
-  margin: auto;
+.card {
+  width: 100%;
+  padding: 1em 1.5em;
+  border-radius: 12px;
+  border: 2px solid black;
+  margin-bottom: 1em;
+}
+.card-image {
+  width: 30px;
+  height: 30px;
+}
+.ul {
+  margin-top: 1em;
+}
+.li {
+  font-size: 0.8rem;
 }
 </style>
