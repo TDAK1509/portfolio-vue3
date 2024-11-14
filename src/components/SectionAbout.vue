1ef1f279f912808e5a9b545ea8c00d390d96316b
Khuong Tran <dbcgdhh@gmail.com>
2024-11-14 14:51:26 +0700

    Style avatar

--- src/components/SectionAbout.vue
+++ src/components/SectionAbout.vue
@@ -1,11 +1,37 @@
 <template>
-  <section class="about">about</section>
+  <section class="about">
+    <div class="image-container">
+      <img class="avatar" src="@/assets/avatar.jpeg" alt="avatar" />
+    </div>
+    <div class="text-container"></div>
+  </section>
 </template>
 
 <style scoped>
 .about {
   background-color: var(--color-white);
-  width: min(900px, 90%);
+  width: 90%;
+  max-width: 900px;
   height: 500px;
+  display: flex;
+}
+.image-container {
+  flex: 1;
+  position: relative;
+}
+.avatar {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+  position: relative;
+  border-radius: 50%;
+  border: 1px solid var(--color-primary);
+}
+.text-container {
+  flex: 1;
 }
 </style>
