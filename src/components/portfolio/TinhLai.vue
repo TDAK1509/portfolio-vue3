f50ff6bdfc608472ed7c4f23af16266ff5e3d7eb
Khuong Tran <dbcgdhh@gmail.com>
2024-11-18 20:51:57 +0700

    Init porfolio tinhlai

---
+++ src/components/portfolio/TinhLai.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="portfolio-item">
+    <div class="portfolio__link">
+      <div>Website:</div>
+      <a
+        href="https://www.tinhlai.com/en"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        https://www.tinhlai.com/en
+      </a>
+
+      <div>Github repository:</div>
+      <a
+        href="https://github.com/TDAK1509/tinhlai"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        https://github.com/TDAK1509/tinhlai
+      </a>
+    </div>
+
+    <div class="portfolio__description">
+      <p>A multilingual tool for calculating compound interest.</p>
+      <p>Technologies used: NuxtJS, VueJS, Tailwind, Netlify.</p>
+    </div>
+
+    <div class="portfolio__images">
+      <div class="images__mobile">
+        <figure class="portfolio_figure">
+          <img
+            class="portfolio_img"
+            src="@/assets/portfolio/tinh-lai-mobile.png"
+            alt="mobile"
+          />
+          <figcaption>Form</figcaption>
+        </figure>
+
+        <figure class="portfolio_figure">
+          <img
+            class="portfolio_img"
+            src="@/assets/portfolio/tinh-lai-result-mobile.png"
+            alt="mobile"
+          />
+          <figcaption>Result</figcaption>
+        </figure>
+      </div>
+
+      <div class="images__desktop">
+        <figure class="portfolio_figure">
+          <img
+            class="portfolio_img"
+            src="@/assets/portfolio/tinh-lai-desktop.png"
+            alt="desktop"
+          />
+          <figcaption>Form</figcaption>
+        </figure>
+
+        <figure class="portfolio_figure">
+          <img
+            class="portfolio_img"
+            src="@/assets/portfolio/tinh-lai-desktop.png"
+            alt="desktop"
+          />
+          <figcaption>Result</figcaption>
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.portfolio_figure {
+  display: inline-block;
+  width: 100%;
+  height: 100px;
+}
+
+.portfolio_img {
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
--- src/views/PortfolioPage.vue
+++ src/views/PortfolioPage.vue
@@ -1,14 +1,20 @@
 <template>
-  <AppPanel>
-    <section class="portfolio-page">portfolio page</section>
+  <AppPanel class="portfolio-container">
+    <section class="portfolio-page">
+      <TinhLai />
+    </section>
   </AppPanel>
 </template>
 
 <script setup>
 import AppPanel from "@/components/AppPanel.vue";
+import TinhLai from "@/components/portfolio/TinhLai.vue";
 </script>
 
 <style scoped>
+.portfolio-container {
+  height: max(85vh, 700px);
+}
 .portfolio-page {
   position: relative;
 }
