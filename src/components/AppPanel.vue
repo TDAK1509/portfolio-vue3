2725b331d541ba2ec31a8897fbebb4047c72a0b3
Khuong Tran <dbcgdhh@gmail.com>
2024-11-22 13:51:24 +0700

    Update padding of app panel

--- src/components/AppPanel.vue
+++ src/components/AppPanel.vue
@@ -10,7 +10,7 @@
   width: 100%;
   max-width: min(90%, 1000px);
   background-color: var(--color-white);
-  padding: 2em;
+  padding: 1.5em;
   box-shadow: 8px 8px 10px 0px rgba(0, 0, 0, 0.3);
   position: relative;
 }
