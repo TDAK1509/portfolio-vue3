7fa865772b37bc1698b1c3870d569fe0ace4bdef
Khuong Tran <dbcgdhh@gmail.com>
2024-11-30 09:36:50 +0700

    Init skills page

--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -1,9 +1,35 @@
 <template>
-  <section class="skills-page">Skills page</section>
+  <AppPanel class="skills-page">
+    <section class="skills-page-section">
+      <form class="skills-page__form" @submit.prevent="search">
+        <input v-model="searchText" type="text" />
+        <button class="form__button">Search</button>
+      </form>
+
+      <ul class="skills-page__skills">
+        <li>VueJS</li>
+        <li>Typescript</li>
+      </ul>
+    </section>
+  </AppPanel>
 </template>
+
+<script setup>
+import AppPanel from "@/components/AppPanel.vue";
+import { ref } from "vue";
+
+const searchText = ref("");
+
+function search() {
+  console.log(searchText.value);
+}
+</script>
 
 <style scoped>
 .skills-page {
+  height: 500px;
+}
+.skills-page-section {
   width: 100%;
   height: 100%;
   position: relative;
